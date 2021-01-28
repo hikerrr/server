83afbb4fcd45a367b8dad9f27dc1b26c8ce200c5
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2021-01-28 22:48:06 +0500

    remove dep: regenerator runtime

--- src/app.js
+++ src/app.js
@@ -1,56 +1,56 @@
 // needed by babel for async and generator functions of ES6
 import "regenerator-runtime/runtime.js";
-import createError from 'http-errors';
-import express from 'express';
-import helmet from 'helmet';
-import path from 'path';
-import logger from 'morgan';
-import indexRouter from './routes/index';
-import destRouter from './routes/destinations';
-import substylesRouter from './routes/substyles';
-import toursRouter from './routes/tours';
-import stylesRouter from './routes/styles';
-import blogsRouter from './routes/blogs';
-import termsRouter from './routes/terms';
-import careersRouter from './routes/careers';
-import contactsRouter from './routes/contacts';
+
+import createError from "http-errors";
+import express from "express";
+import helmet from "helmet";
+import path from "path";
+import logger from "morgan";
+import indexRouter from "./routes/index";
+import destRouter from "./routes/destinations";
+import substylesRouter from "./routes/substyles";
+import toursRouter from "./routes/tours";
+import stylesRouter from "./routes/styles";
+import blogsRouter from "./routes/blogs";
+import termsRouter from "./routes/terms";
+import careersRouter from "./routes/careers";
+import contactsRouter from "./routes/contacts";
 
 const app = express();
 
-if (app.get('env') == 'development') {
-  app.use(logger('dev'));
+if (app.get("env") == "development") {
+  app.use(logger("dev"));
 }
 
 app.use(helmet());
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use((req,res,next) => {
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
     if (req.secure) {
       next();
-    }
-    else {
+    } else {
       res.redirect(`https://${req.headers.host}${req.url}`);
     }
   });
 }
 
-app.use('/', indexRouter);
-app.use('/destinations', destRouter);
-app.use('/substyles', substylesRouter);
-app.use('/tours', toursRouter);
-app.use('/styles', stylesRouter);
-app.use('/blogs', blogsRouter);
-app.use('/terms', termsRouter);
-app.use('/careers', careersRouter);
-app.use('/contact', contactsRouter);
+app.use("/", indexRouter);
+app.use("/destinations", destRouter);
+app.use("/substyles", substylesRouter);
+app.use("/tours", toursRouter);
+app.use("/styles", stylesRouter);
+app.use("/blogs", blogsRouter);
+app.use("/terms", termsRouter);
+app.use("/careers", careersRouter);
+app.use("/contact", contactsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -61,11 +61,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 export default app;
